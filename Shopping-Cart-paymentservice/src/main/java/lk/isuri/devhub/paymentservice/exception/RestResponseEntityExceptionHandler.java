68b512962c875d4080062be2f528c3bda5ed0b43
isuri <isuriwgunaratne@gmail.com>
2023-12-14 15:13:25 +0530

    Handling Custom Exception

---
+++ Shopping-Cart-paymentservice/src/main/java/lk/isuri/devhub/paymentservice/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,19 @@
+package lk.isuri.devhub.paymentservice.exception;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import lk.isuri.devhub.paymentservice.payload.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(PaymentServiceCustomException.class)
+    public ResponseEntity<ErrorResponse> handleProductServiceException(PaymentServiceCustomException exception) {
+        return new ResponseEntity<>(ErrorResponse.builder()
+                .errorMessage(exception.getMessage())
+                .errorCode(exception.getErrorCode())
+                .build(), HttpStatus.NOT_FOUND);
+    }
+}
