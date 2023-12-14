37afd36f52fe880dbbf15e7821845039dd1f5dbe
isuri <isuriwgunaratne@gmail.com>
2023-12-14 12:01:09 +0530

    Create Custom Exception

---
+++ src/main/java/lk/isuri/devhub/Shopping/productservice/exception/ProductServiceCustomException.java
@@ -0,0 +1,14 @@
+package lk.isuri.devhub.Shopping.productservice.exception;
+
+import lombok.Data;
+
+@Data
+public class ProductServiceCustomException extends RuntimeException{
+
+    private String errorCode;
+
+    public ProductServiceCustomException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
